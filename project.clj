94a580fcbb5a2212b52fa299586a2befcd84b1ab
Zohar Kelrich <zohar@amobee.com>
2018-07-16 12:11:49 +0300

    Version 0.10.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amobee/meeseeks-db "0.10.1"
+(defproject com.amobee/meeseeks-db "0.10.2-SNAPSHOT"
   :description "Redis-based fast sharded set-oriented DB"
   :url "https://github.com/kontera-technologies/meeseeks-db"
   :dependencies [[org.clojure/clojure     "1.9.0"]

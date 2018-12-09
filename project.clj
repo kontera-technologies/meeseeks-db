b55e2e625c4b549e0354dbf7438115dbd9c7054a
Alon Rozental <arozental@amobee.com>
2018-12-09 11:50:30 +0200

    no seqable (clojure 1.9) function 0.10.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amobee/meeseeks-db "0.10.7"
+(defproject com.amobee/meeseeks-db "0.10.8"
   :description "Redis-based fast sharded set-oriented DB"
   :url "https://github.com/kontera-technologies/meeseeks-db"
   :dependencies [[org.clojure/clojure     "1.9.0"]

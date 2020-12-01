3fd542a46f6c55c8102f7e9b91bb7730dc3574e6
amirmamo <amir.mamo@amobee.com>
2020-12-01 15:52:37 +0200

    necessary doall

--- src/meeseeks_db/core.clj
+++ src/meeseeks_db/core.clj
@@ -110,41 +110,41 @@
 
 (defn default-multi-id->iid
   ([conn id_list]
-     (let [save-id (fn [i-id-iid_list]
-                       (when i-id-iid_list
-                         (let [id_list (map :id i-id-iid_list)
-                               iid_list (map :iid i-id-iid_list)
-                               key_list (map #(str "iid:" %) iid_list)
-                               result-id_list (wcar conn (apply car/mget key_list))
-                               nil-key-id_list (filter #(nil? (first %)) (map list result-id_list key_list id_list))
-                               key-id_list (flatten (map (fn [[nil-id key id]] [key id]) nil-key-id_list))]
+   (let [save-id (fn [i-id-iid_list]
+                   (when i-id-iid_list
+                     (let [id_list (map :id i-id-iid_list)
+                           iid_list (map :iid i-id-iid_list)
+                           key_list (map #(str "iid:" %) iid_list)
+                           result-id_list (wcar conn (apply car/mget key_list))
+                           nil-key-id_list (filter #(nil? (first %)) (map list result-id_list key_list id_list))
+                           key-id_list (flatten (map (fn [[nil-id key id]] [key id]) nil-key-id_list))]
 
-                           (when (not-empty key-id_list) (wcar conn (apply car/mset key-id_list))))))
+                       (when (not-empty key-id_list) (wcar conn (apply car/mset key-id_list))))))
 
-           iid_list (wcar conn (apply car/mget (map #(str "id:" %) id_list)))
-           i-id-iid_list (map-indexed (fn [i [id_i iid_i]] {:i i :id id_i :iid iid_i}) (map list id_list iid_list))
-           iid-found?i-id-iid_list (group-by (fn [idx-id-iid] (some? (:iid idx-id-iid))) i-id-iid_list)
-           i-id-exist-iid_list (get iid-found?i-id-iid_list true)
-           i-id-nil_list (get iid-found?i-id-iid_list false)
-           i-id-new-iid_list (let [n-new-iids (count i-id-nil_list)
-                                   last-new-iid (:result (with-lock conn "kona-iid" 20000 50000
-                                                            (wcar conn (car/incrby "next-iid" n-new-iids))))]
-                                 (if
-                                   (and (> n-new-iids 0) last-new-iid)
-                                   (let [idx_list (map :i i-id-nil_list)
-                                         id_list (map :id i-id-nil_list)
-                                         new-iid_list (range (+ 1 (- last-new-iid n-new-iids)) (+ 1 last-new-iid))
+         iid_list (wcar conn (apply car/mget (doall (map #(str "id:" %) id_list))))
+         i-id-iid_list (map-indexed (fn [i [id_i iid_i]] {:i i :id id_i :iid iid_i}) (map list id_list iid_list))
+         iid-found?i-id-iid_list (group-by (fn [idx-id-iid] (some? (:iid idx-id-iid))) i-id-iid_list)
+         i-id-exist-iid_list (get iid-found?i-id-iid_list true)
+         i-id-nil_list (get iid-found?i-id-iid_list false)
+         i-id-new-iid_list (let [n-new-iids (count i-id-nil_list)
+                                 last-new-iid (:result (with-lock conn "kona-iid" 20000 50000
+                                                                  (wcar conn (car/incrby "next-iid" n-new-iids))))]
+                             (if
+                               (and (> n-new-iids 0) last-new-iid)
+                               (let [idx_list (map :i i-id-nil_list)
+                                     id_list (map :id i-id-nil_list)
+                                     new-iid_list (range (+ 1 (- last-new-iid n-new-iids)) (+ 1 last-new-iid))
 
-                                          id:new-iid_list (flatten (map list (map #(str "id:" %) id_list) new-iid_list))
-                                          new-iid:id_list (flatten (map list (map #(str "iid:" %) new-iid_list) id_list))]
-                                          (wcar conn (apply car/mset (concat id:new-iid_list new-iid:id_list)))
+                                     id:new-iid_list (flatten (map list (map #(str "id:" %) id_list) new-iid_list))
+                                     new-iid:id_list (flatten (map list (map #(str "iid:" %) new-iid_list) id_list))]
+                                 (wcar conn (apply car/mset (concat id:new-iid_list new-iid:id_list)))
 
-                                          (map (fn [[idx id new-iid]] {:i idx :id id :iid new-iid})
-                                               (map list idx_list id_list new-iid_list)))
-                                   i-id-nil_list))]
+                                 (map (fn [[idx id new-iid]] {:i idx :id id :iid new-iid})
+                                      (map list idx_list id_list new-iid_list)))
+                               i-id-nil_list))]
 
-   (save-id i-id-exist-iid_list)
-   (map :iid (sort-by :i (concat i-id-exist-iid_list i-id-new-iid_list)))))
+     (save-id i-id-exist-iid_list)
+     (map :iid (sort-by :i (concat i-id-exist-iid_list i-id-new-iid_list)))))
 
   ([conn id delete?]
    (wcar conn (doseq [id_i id] (car/get (str "id:" id_i))))))
@@ -260,49 +260,49 @@
   (let [db          (deref db)
         data-db     (deref data-db)
         conn-dconn->obj_list (let [id_list (map :id obj_list)
-                                  conn_list (map #(f-id->conn db %) id_list)
-                                  data-conn_list (map #(f-id->conn data-db %) id_list)]
-                        (group-by-idx (fn [idx] [(nth conn_list idx) (nth data-conn_list idx)]) obj_list))
+                                   conn_list (doall (map #(f-id->conn db %) id_list))
+                                   data-conn_list (doall (map #(f-id->conn data-db %) id_list))]
+                               (group-by-idx (fn [idx] [(nth conn_list idx) (nth data-conn_list idx)]) obj_list))
         todo (map (fn [[[conn data-conn] obj]]
                     (let [id (map :id obj)
                           iid (f-multi-id->iid conn id)
-                          old (wcar data-conn (map #(fetch-object %) id))
+                          old (wcar data-conn (doall (map #(fetch-object %) id)))
                           old (flatten (conj [] old))
 
-                          index-pairs (map
+                          index-pairs (doall (map
                                                (fn [[old_i obj_i]]
-                                                  (merge-with conj
-                                                    (->> (indexify f-index old_i)
-                                                         (map (fn [[k vs]] [k {:old vs}]))
-                                                         (into {}))
-                                                    (->> (indexify f-index obj_i)
-                                                         (map (fn [[k vs]] [k {:new vs}]))
-                                                       (into {}))))
-                                               (map list old obj))
+                                                 (merge-with conj
+                                                             (->> (indexify f-index old_i)
+                                                                  (map (fn [[k vs]] [k {:old vs}]))
+                                                                  (into {}))
+                                                             (->> (indexify f-index obj_i)
+                                                                  (map (fn [[k vs]] [k {:new vs}]))
+                                                                  (into {}))))
+                                               (map list old obj)))
                           ]
                       [conn data-conn obj iid index-pairs]))
                   (seq conn-dconn->obj_list))]
 
     (doseq [[conn data-conn obj_list iid_list index-pairs_list] todo]
       (let [iid-id_list (map list iid_list (map :id obj_list))
-            result-id_list (wcar conn (apply car/mget (map #(str "iid:" %) iid_list)))
-            iid->id (flatten (map first (filter (fn [[iid-id id?]] (nil? id?))
-                                             (map list iid-id_list result-id_list))))
+            result-id_list (wcar conn (apply car/mget (doall (map #(str "iid:" %) iid_list))))
+            iid->id (doall (flatten (map first (filter (fn [[iid-id id?]] (nil? id?))
+                                                       (map list iid-id_list result-id_list)))))
             iid-index-pairs (filter
                               (fn [[iid_i index-pairs_i]] (and iid_i (not-empty index-pairs_i)))
                               (map list iid_list index-pairs_list))
-            iid_list (map first iid-index-pairs)]
+            iid_list (doall (map first iid-index-pairs))]
 
-            (when (seq iid->id) (wcar conn (apply car/mset iid->id)))
+        (when (seq iid->id) (wcar conn (apply car/mset iid->id)))
 
-            (doseq [[iid_i index-pairs_i] iid-index-pairs]
-               (wcar conn
-                 (doseq [[k {:keys [old_i new_i]}] index-pairs_i]
-                   (update-attr! iid_i k old_i new_i))))
+        (doseq [[iid_i index-pairs_i] iid-index-pairs]
+          (wcar conn
+                (doseq [[k {:keys [old_i new_i]}] index-pairs_i]
+                  (update-attr! iid_i k old_i new_i))))
 
-            (when (seq iid_list) (wcar conn (apply car/sadd "total" iid_list)))
+        (when (seq iid_list) (wcar conn (apply car/sadd "total" iid_list)))
 
-            (wcar data-conn
+        (wcar data-conn
               (doseq [obj_i obj_list]
                 (save-object! (:id obj_i) obj_i)))))))
 
@@ -311,19 +311,19 @@
   [{:keys [db data-db f-id->conn f-multi-id->iid f-index]} id_list]
   (let [db        @db
         data-db   @data-db
-        conn-dconn->id_list (let [conn (map #(f-id->conn db %) id_list)
-                            data-conn (map #(f-id->conn data-db %) id_list)]
-                        (group-by-idx (fn [idx] [(nth conn idx) (nth data-conn idx)]) id_list))
+        conn-dconn->id_list (let [conn (doall (map #(f-id->conn db %) id_list))
+                                  data-conn (doall (map #(f-id->conn data-db %) id_list))]
+                              (group-by-idx (fn [idx] [(nth conn idx) (nth data-conn idx)]) id_list))
 
-        conn-dconn->id_list-iid_list-indices_list (map
-                  (fn [[[conn data-conn] id-list]]
-                    (let [iid-list (flatten (conj [] (f-multi-id->iid conn id-list "delete")))
+        conn-dconn->id_list-iid_list-indices_list (doall (map
+                                                           (fn [[[conn data-conn] id-list]]
+                                                             (let [iid-list (flatten (conj [] (f-multi-id->iid conn id-list "delete")))
 
-                          obj_list (flatten (conj [] (wcar data-conn (map #(fetch-object %) id-list))))
-                          indices_list (map #(indexify f-index %) obj_list)]
+                                                                   obj_list (flatten (conj [] (wcar data-conn (map #(fetch-object %) id-list))))
+                                                                   indices_list (map #(indexify f-index %) obj_list)]
 
-                      [conn data-conn id-list iid-list indices_list]))
-                  (seq conn-dconn->id_list))]
+                                                               [conn data-conn id-list iid-list indices_list]))
+                                                           (seq conn-dconn->id_list)))]
 
     (doseq [[conn data-conn id_list iid-list indices_list] conn-dconn->id_list-iid_list-indices_list]
       (wcar conn
@@ -333,8 +333,8 @@
             (apply car/srem "total" iid-list))
 
       (wcar data-conn
-        (doseq [id_i id_list]
-          (save-object! id_i nil))))))
+            (doseq [id_i id_list]
+              (save-object! id_i nil))))))
 
 (defn fetch
   "Fetch object by ID"
@@ -352,12 +352,12 @@
         id->conn (:f-id->conn client)
         db-buckets (group-by #(id->conn db %) ids)]
     (doall (pmap (fn [[db ids]]
-            (when (not-empty ids)
-              (let [iids (wcar db (apply car/mget (map #(str "id:" %) ids)))
-                    key-name (str "custom:" (name attribute-name))]
-                (wcar db
-                      (apply car/sadd key-name iids)
-                      (car/expire key-name 3600))))) db-buckets))))
+                   (when (not-empty ids)
+                     (let [iids (wcar db (apply car/mget (map #(str "id:" %) ids)))
+                           key-name (str "custom:" (name attribute-name))]
+                       (wcar db
+                             (apply car/sadd key-name iids)
+                             (car/expire key-name 3600))))) db-buckets))))
 
 (defn fix-custom-keys* [query mangle-map]
   (if (empty? (:nested query))
@@ -396,15 +396,15 @@
   (let [db (deref db)
         data-db (deref data-db)]
     (dorun
-     (pmap #(wcar % (car/flushdb)) db))
+      (pmap #(wcar % (car/flushdb)) db))
     (dorun
-     (pmap (fn [conn]
-             (scan-indices* conn "p:*"
-                            (fn [ks]
-                              (when (seq ks)
-                                (wcar conn
-                                      (apply car/del ks))))))
-           data-db))))
+      (pmap (fn [conn]
+              (scan-indices* conn "p:*"
+                             (fn [ks]
+                               (when (seq ks)
+                                 (wcar conn
+                                       (apply car/del ks))))))
+            data-db))))
 
 (defn memory-status [{:keys [db data-db]}]
   (letfn [(status [conn]
@@ -417,8 +417,8 @@
               {:capacity max-memory
                :used     used-memory}))
           (aggregate [statuses]
-                     {:raw      statuses
-                      :capacity (reduce + (map :capacity statuses))
-                      :used     (reduce + (map :used     statuses))})]
+            {:raw      statuses
+             :capacity (reduce + (map :capacity statuses))
+             :used     (reduce + (map :used     statuses))})]
     {:db      (aggregate (pmap status db))
      :data-db (aggregate (pmap status data-db))}))

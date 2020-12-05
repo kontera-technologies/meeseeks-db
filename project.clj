2f13a9a547d1b4d4009bd673aa05ce35cace104b
amirmamo <amir.mamo@amobee.com>
2020-12-05 20:16:04 +0200

    change project path

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amobee/meeseeks-db "0.20.00"
+(defproject meeseeks-db "0.20.00"
   :description "Redis-based fast sharded set-oriented DB"
   :url "https://github.com/kontera-technologies/meeseeks-db"
   :dependencies [[org.clojure/clojure     "1.9.0"]
